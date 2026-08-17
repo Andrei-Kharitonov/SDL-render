c12336f77d38ca106640fa371763068c3bdcc278
Andrei-Kharitonov <89729599+Andrei-Kharitonov@users.noreply.github.com>
2026-08-18 03:30:41 +0400

    render borders

--- render.c
+++ render.c
@@ -30,10 +30,17 @@ typedef struct {
 void draw_rectangle(Rectangle *rectangle) {
   for (int j = 0; j < HEIGHT; j++) {
     for (int i = 0; i < WIDTH; i++) {
-      int w = fabs(rectangle->position.x - i) <= rectangle->width/2;
-      int h = fabs(rectangle->position.y - j) <= rectangle->height/2;
-      if (w && h) {
+      int h = fabs(rectangle->position.x - i) <= rectangle->width/2 - 1;
+      int v = fabs(rectangle->position.y - j) <= rectangle->height/2 - 1;
+      int h_border = fabs(rectangle->position.x - i) <= rectangle->width/2;
+      int v_border = fabs(rectangle->position.y - j) <= rectangle->height/2;
+      // fill
+      if (h && v) {
         paint_pixel(i, j, rectangle->color);
+      }
+      // edge
+      if ((h_border && v_border) && !(h && v)) {
+        paint_pixel(i, j, GREEN);
       }
     }
   }
@@ -45,13 +52,13 @@ void draw_circle(Circle *circle) {
       double x_sq = pow(circle->position.x - i, 2);
       double y_sq = pow(circle->position.y - j, 2);
       double r_sq = pow(circle->radius, 2);
-      // edge
-      if (x_sq + y_sq <= r_sq) {
-        paint_pixel(i, j, GREEN);
-      }
       // fill
       if (!(x_sq + y_sq + circle->radius*2 > r_sq)) {
         paint_pixel(i, j, circle->color);
+      }
+      // edge
+      if ((x_sq + y_sq <= r_sq) && (x_sq + y_sq + circle->radius*2 > r_sq)) {
+        paint_pixel(i, j, GREEN);
       }
     }
   }
@@ -64,16 +71,17 @@ void move_obj(Point *point, double delta_time) {
 
 Rectangle rect = {
   {20, 80},
-  {10, 2},
+  {35, -41},
   WHITE,
-  16,
-  8
+  32,
+  20
 };
+
 Circle ball = {
   {WIDTH/2.0, HEIGHT/2.0},
-  {57, 23},
+  {53, 27},
   WHITE,
-  20,
+  20
 };
 
 
@@ -81,14 +89,22 @@ void my_render(double delta_time) {
   draw_rectangle(&rect);
   draw_circle(&ball);
 
-  int is_top = ball.position.y - ball.radius <= 0;
-  int is_bottom = ball.position.y + ball.radius >= HEIGHT;
-  int is_left = ball.position.x - ball.radius <= 0;
-  int is_right = ball.position.x + ball.radius >= WIDTH;
+  int c_is_top = ball.position.y - ball.radius <= 0;
+  int c_is_bottom = ball.position.y + ball.radius >= HEIGHT;
+  int c_is_left = ball.position.x - ball.radius <= 0;
+  int c_is_right = ball.position.x + ball.radius >= WIDTH;
 
-  if (is_top || is_bottom) ball.velocity.y = -ball.velocity.y;
-  if (is_left || is_right) ball.velocity.x = -ball.velocity.x;
+  int r_is_top = rect.position.y - rect.height/2 <= 0;
+  int r_is_bottom = rect.position.y + rect.height/2 >= HEIGHT;
+  int r_is_left = rect.position.x - rect.width/2 <= 0;
+  int r_is_right = rect.position.x + rect.width/2 >= WIDTH;
 
-  move_obj((Point *)&rect, delta_time);
+  if (c_is_top || c_is_bottom) ball.velocity.y = -ball.velocity.y;
+  if (c_is_left || c_is_right) ball.velocity.x = -ball.velocity.x;
+
+  if (r_is_top || r_is_bottom) rect.velocity.y = -rect.velocity.y;
+  if (r_is_left || r_is_right) rect.velocity.x = -rect.velocity.x;
+
   move_obj((Point *)&ball, delta_time);
+  move_obj((Point *)&rect, delta_time);
 }
